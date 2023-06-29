cb788d37b8cb10573eeb5814b3873d58fb4c0092
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-06-29 15:50:18 +0200

    turn on eureka

--- src/main/java/com/smarthome/shmainservice/ShMainServiceApplication.java
+++ src/main/java/com/smarthome/shmainservice/ShMainServiceApplication.java
@@ -2,10 +2,11 @@ package com.smarthome.shmainservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableJpaRepositories("com.smarthome.shmainservice.repo")
 public class ShMainServiceApplication {
 
