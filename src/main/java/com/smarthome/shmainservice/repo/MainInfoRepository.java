1d9eab1ec1752ea31efc4888f10e02a3f9c36f01
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-07-01 11:08:50 +0200

    add @Repository

--- src/main/java/com/smarthome/shmainservice/repo/MainInfoRepository.java
+++ src/main/java/com/smarthome/shmainservice/repo/MainInfoRepository.java
@@ -2,9 +2,11 @@ package com.smarthome.shmainservice.repo;
 
 import com.smarthome.shmainservice.entity.MainInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MainInfoRepository extends JpaRepository<MainInfo, Long> {
 
     Optional<MainInfo> findTop1ByOrderByIdDesc();
--- src/main/java/com/smarthome/shmainservice/repo/PostRepository.java
+++ src/main/java/com/smarthome/shmainservice/repo/PostRepository.java
@@ -4,19 +4,16 @@ import com.smarthome.shmainservice.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query(value = "SELECT * FROM post ORDER BY post_id DESC LIMIT :count", nativeQuery = true)
     List<Post> findLastPosts(@Param("count") Integer count);
-
-//    @Query(
-//            value = "SELECT * FROM Users u WHERE u.status = ?1",
-//            nativeQuery = true)
-//    User findUserByStatusNative(Integer status);
 
     Optional<Post> findTop1ByOrderByIdDesc();
 }
